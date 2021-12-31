b721febbdad3c70966f1a6e2359b8de9677e7b7d
Keith Chow <18042844+GassiusODude@users.noreply.github.com>
2021-12-30 20:45:12 -0500

    Switch to tensorboard to review results

--- examples/MNIST_example/example.py
+++ examples/MNIST_example/example.py
@@ -1,6 +1,13 @@
 """MNIST database of handwritten digits
 
-Based on https://nextjournal.com/gkoehler/pytorch-mnist
+
+References
+----------
+.. [1] G. Koehler, "MNIST Handwritten Digit Recognition in PyTorch",
+    2020, Available: 
+    https://nextjournal.com/gkoehler/pytorch-mnist
+.. [2] PyTorch, "Torch.Utils.Tensorboard",
+    https://pytorch.org/docs/stable/tensorboard.html
 """
 import torch
 import torchvision
@@ -8,7 +15,7 @@ from matplotlib import pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+from torch.utils.tensorboard import SummaryWriter
 
 batch_size_train = 64
 batch_size_test = 1000
@@ -24,12 +31,12 @@ train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST(
        'files/', train=True, download=True,
         transform=im_to_tensor),
-    batch_size=batch_size_train, shuffle=True)
+    batch_size=batch_size_train, shuffle=True, num_workers=6)
 
 test_loader = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST('files/', train=False, download=True,
         transform=im_to_tensor),
-    batch_size=batch_size_test, shuffle=True)
+    batch_size=batch_size_test, shuffle=True, num_workers=6)
 
 log_interval = 10
 
@@ -37,8 +44,17 @@ log_interval = 10
 
 
 # ---------------------------  design model  --------------------------------
-
 class MyModel(nn.Module):
+    """
+    Model is from [1_].
+
+    My interpretation of what it is doing is applying 5x5 kernel and 
+    downsampling by 2.  Followed by a second stage of kernel and 
+    downsample.  The output is then pushed through a dense layer
+    from 320 samples -> 50, followed by a second dense layer bringing
+    the samples down from 50 to 10.  The last layer is a softmax
+    to choose the highest of 10 classification labels (0-9)
+    """
     def __init__(self):
         super(MyModel, self).__init__()
         # expand 10 fold and apply kernel of size 5
@@ -72,12 +88,13 @@ class MyModel(nn.Module):
         x = self.fc2(x)
 
         # [batch, 10] (10 classes from 0 - 9)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
     
 # ---------------------------  train model  ---------------------------------
-def train(model, epoch, device=torch.device('cpu')):
+def train(model, epoch, device=torch.device('cpu'), writer=None):
     #model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
         # move data to device
         data = data.to(device)
         target = target.to(device)
@@ -86,27 +103,19 @@ def train(model, epoch, device=torch.device('cpu')):
         output = model(data)
 
         # compute neg log likelihood loss
-        optimizer.zero_grad()
+        
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
 
-        # print training loss (based on log interval)
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-            epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+    if writer:
+        writer.add_scalar("Loss/train", loss.item(), epoch)
+    
+    # save network and optimizer state
+    torch.save(model.state_dict(), 'results/model.pth')
+    torch.save(optimizer.state_dict(), 'results/optimizer.pth')
 
-        # track training loss
-        train_losses.append(loss.item())
-        train_counter.append(
-            (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
-        
-        # save network and optimizer state
-        torch.save(model.state_dict(), 'results/model.pth')
-        torch.save(optimizer.state_dict(), 'results/optimizer.pth')
-
-def test(model, device=torch.device('cpu')):
+def test(model, epoch, device=torch.device('cpu'), writer=None):
     #model.test()
     test_loss = 0
     correct = 0
@@ -120,51 +129,51 @@ def test(model, device=torch.device('cpu')):
             output = model(data)
 
             # calculate loss
-            test_loss += F.nll_loss(output, target, size_average=False).item()
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
 
-        test_loss /= len(test_loader.dataset)
-        test_losses.append(test_loss)
-        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
+        acc = 100. * correct / len(test_loader.dataset)
+
+        if writer:
+            writer.add_scalar("Accuracy/test", acc, epoch)
         
 
 if __name__ == "__main__":
+    # -------------------------  parse arguments  ---------------------------
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("--epochs", default=3, type=int, help="Num epochs")
     parser.add_argument("--lr", default=1e-2, type=float, help="Learn rate")
     parser.add_argument("--momentum", default=0.5, type=float, help="Momentum")
+    parser.add_argument(
+        "--opt", default=0, choices=range(2), type=int, help="0=SGD, 1=Adam")
     parser.add_argument("--device", default="cuda")
     args = parser.parse_args()
+    
+    # setup device
     if args.device == "cuda":
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     else:
         device = torch.device("cpu")
 
-    train_losses = []
-    train_counter = []
-    test_losses = []
-    test_counter =\
-        [i * len(train_loader.dataset) for i in range(args.epochs +1 )]
-        
+    # setup summary writer
+    comment = "%s_%d_%0.4f" % ("Model1", args.opt, args.lr)
+    writer = SummaryWriter(comment=comment)
+
     # -------------------- initialize model and optimizer  ------------------
     network = MyModel()
     network.to(device)
-    optimizer = optim.SGD(
-        network.parameters(), lr=args.lr, momentum=args.momentum)
+    if args.opt == 0:
+        optimizer = optim.SGD(
+            network.parameters(), lr=args.lr, momentum=args.momentum)
+    
+    else:
+        optimizer = optim.Adam(network.parameters(), lr=args.lr)
 
-    test(network, device)
+    test(network, 0, device, writer=writer)
     for epoch in range(1, args.epochs + 1):
-        train(network, epoch, device)
-        test(network, device)
+        train(network, epoch, device,writer=writer)
+        test(network, epoch, device, writer=writer)
 
-    fig = plt.figure()
-    plt.plot(train_counter, train_losses, color='blue')
-    plt.scatter(test_counter, test_losses, color='red')
-    plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-    plt.xlabel('number of training examples seen')
-    plt.ylabel('negative log likelihood loss')
-    plt.show()
+    writer.close()
